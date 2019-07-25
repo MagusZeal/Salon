33fd6bc0efe5d7090b909579d0ea6a9f6fd074a7
Raúl <raulsandocan@gmail.com>
2019-07-25 19:25:02 -0400

    deploy 0

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+
+  firebase:{
+    apiKey: "AIzaSyABeJBa3p-0ulwYmnAQ3vZBJIzq9YQd6rQ",
+    authDomain: "salonsw-e6485.firebaseapp.com",
+    databaseURL: "https://salonsw-e6485.firebaseio.com",
+    projectId: "salonsw-e6485",
+    storageBucket: "salonsw-e6485.appspot.com",
+    messagingSenderId: "73580368596",
+    appId: "1:73580368596:web:f06f2b31561e9f21"
+    
+  }
 };
